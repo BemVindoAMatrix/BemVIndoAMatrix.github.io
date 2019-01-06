f1400ca2206e139afd836e0ac24470db2492c381
UserUnavailable <klebersilvanaruto6@gmail.com>
2019-01-06 19:08:25 -0200

    position fixed, infobox

--- javascript.js
+++ javascript.js
@@ -13,3 +13,13 @@ if (window.location.pathname == "/") {
 } else if (window.location.pathname == "/matrixintl.html") {
           window.onload = postIt("ENposts.txt");
 }
+
+window.onscroll = function infobox() {
+          if (window.scrollY >= 195) {
+                    document.getElementById("infobox").style.position = "fixed";
+                    document.getElementById("infobox").style.top = "95px";
+          } else {
+                    document.getElementById("infobox").style.position = "absolute";
+                    document.getElementById("infobox").style.top = "290px";
+          }
+}
